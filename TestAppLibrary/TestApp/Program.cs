f9b532443a6138626958f0e8f3d727d23e84046e
Android2309 <andr.frsv@yandex.ru>
2021-07-30 19:19:39 +0300

    testWork

---
+++ TestAppLibrary/TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TestAppLibrary;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = "Этот текст (строка) предназначен для тестирования [бы{стр}ой] проверки вхождения скобок {любых [из трех видов]}";
+            string text2 = "[]({}))[]";
+            string text3 = "[{(})]";
+
+            Console.WriteLine(text1);
+            Console.WriteLine(text1.CheckString());
+            Console.WriteLine(text2.CheckString());
+            Console.WriteLine(text3.CheckString());
+        }
+    }
+}
---
+++ TestAppLibrary/TestAppLibrary/Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestAppLibrary
+{
+    public static class StringExtension
+    {
+        public static bool CheckString(this string text)
+        {
+            bool result = true;
+
+            //словарь проверяемых символов
+            Dictionary<char, int[]> keys = new Dictionary<char, int[]>
+            {
+                ['('] = new int[] { 1, 1 },
+                [')'] = new int[] { 1, 0 },
+                ['{'] = new int[] { 2, 1 },
+                ['}'] = new int[] { 2, 0 },
+                ['['] = new int[] { 3, 1 },
+                [']'] = new int[] { 3, 0 }
+            };
+
+            //строка со скобками
+            string braces = "";
+
+            //убираем все кроме скобок
+            foreach (var c in text)
+            {
+                foreach (var key in keys)
+                {
+                    if (c == key.Key)
+                    {
+                        braces += c;
+                    }
+                }
+            }
+
+            //проверяем скобки на валидность
+            for (int i = 0; i < braces.Length; i++)
+            {
+                foreach (var key in keys)
+                {
+                    //проверка является ли скобка закрывающей
+                    if (braces[i] == key.Key && key.Value[1] == 0)
+                    {
+                        //проверка является ли закрывающая скобка такого же семейства как предыдущая
+                        if (keys.Where(k => k.Key == braces[i - 1]).FirstOrDefault().Value[0] == key.Value[0])
+                        {
+                            braces = braces.Remove(i - 1, 2);
+                            i = 0;
+                            break;
+                        }
+                        else return false;
+                    }
+                }
+            }
+
+            if (braces != "") return false;
+            return result;
+        }
+    }
+}
